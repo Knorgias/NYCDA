0e76eb7182b6d61ac24ea9de85de7f84b109d21a
Kostas Norgias <kostasnorgias@gmail.com>
2017-01-19 16:19:22 +0100

    experimental changes

--- Web Dev/js/custom.js
+++ Web Dev/js/custom.js
@@ -1,5 +1,25 @@
-$('#firstQuestion').click(
-	function(){
-		$('#firstAnswer').toggle(800);
-	}
-);
+var navPositionY = $('.menubar').offset().top;
+
+function logScroll(){
+
+console.log($(window).scrollTop());		
+console.info ($('.square').offset().top);
+  
+  if ($(window).scrollTop() >= navPositionY){
+     //console.info('DOWN square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
+    $('.menubar')
+      .css ('position','fixed')
+      .css('bottom','0')
+      .css ('left','0')
+  }
+  else{
+     //console.info('UP square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
+    $('.menubar')
+     .css ('position','relative')
+   
+  }
+}
+
+
+
+ window.onscroll = logScroll;
