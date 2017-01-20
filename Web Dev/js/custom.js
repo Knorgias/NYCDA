2f70baeac0d8fa9fd17c6f37f6fb356ab33554cf
Kostas Norgias <kostasnorgias@gmail.com>
2017-01-20 16:07:37 +0100

    from.course.20-01-2017

--- Web Dev/js/custom.js
+++ Web Dev/js/custom.js
@@ -1,25 +1,64 @@
+ //Make menu bar fixed after scroll
 var navPositionY = $('.menubar').offset().top;
 
-function logScroll(){
+function logScroll() {
 
-console.log($(window).scrollTop());		
-console.info ($('.square').offset().top);
-  
-  if ($(window).scrollTop() >= navPositionY){
-     //console.info('DOWN square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
+  if ($(window).scrollTop() >= navPositionY) {
+    //console.info('DOWN square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
     $('.menubar')
-      .css ('position','fixed')
-      .css('bottom','0')
-      .css ('left','0')
-  }
-  else{
-     //console.info('UP square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
+      .css('position', 'fixed')
+      .css('top', '0')
+      .css('left', '0')
+      .css('width', '100%')
+  } else {
+    //console.info('UP square '+ squarePositionY+ ' window scroll:' +$(window).scrollTop() );
     $('.menubar')
-     .css ('position','relative')
-   
+      .css('position', 'relative')
+
   }
 }
 
+window.onscroll = logScroll;
+//Make menu bar fixed after scroll-END
 
+ //Expand-collapse images
+ var currentImage;
+ var counter = 0;
 
- window.onscroll = logScroll;
+ $( ".galleryImage" ).click(function(event) {
+   //console.info (e);
+   if(counter%2 == 0){
+      $(event.target)
+      .css('position','fixed')
+      .css('z-index', '2')
+      .css('cursor', 'pointer')
+
+      .animate({
+         width:"70%",
+         height:"70%",
+         margin:0,
+         top:0,
+         left:0,
+
+       }, 900)
+      .clearQueue();
+      counter += 1;
+  } else {
+      $(event.target)
+      .css('position', 'relative')
+      .css('z-index', '1')
+      .css('cursor', 'pointer')
+
+      .animate({
+        width:"250px",
+        height: "250px",
+      }, 900);
+      .clearQueue();
+      counter += 1;
+  }
+ });
+ //Expand-collapse images-END
+
+ //Experimental
+
+ //Experimental-END
